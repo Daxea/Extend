6ea494ff65e3e469a6ae9517f2a943d00579de9d
Eddy Pinkston <eddy.ews@gmail.com>
2017-04-19 16:00:39 -0500

    Add Int32 extensions

---
+++ Extend.Tests/Int32Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Extend.Tests
+{
+    [TestClass]
+    public class Int32Tests
+    {
+        [TestMethod]
+        public void Pow_2x3()
+        {
+            Assert.AreEqual(8, 2.Pow(3));
+        }
+
+        [TestMethod]
+        public void Pow_3x3()
+        {
+            Assert.AreEqual(27, 3.Pow(3));
+        }
+
+        [TestMethod]
+        public void Sqr_2()
+        {
+            Assert.AreEqual(4, 2.Sqr());
+        }
+
+        [TestMethod]
+        public void Sqrt_1024()
+        {
+            Assert.AreEqual(32, 1024.Sqrt());
+        }
+    }
+}
---
+++ Extend/Int32s.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extend
+{
+    public static class Int32s
+    {
+        public static int Pow(this int number, int exponent) => (int)Math.Pow(number, exponent);
+
+        public static int Sqr(this int number) => number * number;
+
+        public static double Sqrt(this int number) => Math.Sqrt(number);
+    }
+}
